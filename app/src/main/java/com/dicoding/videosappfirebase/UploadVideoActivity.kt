601c2ca3cecc04b6f8579c2ff47aafc111c9bb11
gregoriuswisnu <wisnu.gregorius1@gmail.com>
2021-05-18 20:44:44 +0700

    upload

--- app/src/main/java/com/dicoding/videosappfirebase/UploadVideoActivity.kt
+++ app/src/main/java/com/dicoding/videosappfirebase/UploadVideoActivity.kt
@@ -68,7 +68,7 @@ class UploadVideoActivity : AppCompatActivity() {
         storageReference.putFile(videoUri!!)
             .addOnSuccessListener {
                 val uriTask = it.storage.downloadUrl
-//                while (!uriTask.isSuccessful);
+                while (!uriTask.isSuccessful);
                 val downloadUri = uriTask.result
                 if (uriTask.isSuccessful){
                     val hashMap = HashMap<String, Any>()
@@ -78,7 +78,7 @@ class UploadVideoActivity : AppCompatActivity() {
                     hashMap["deepfake"] = "$deepfake"
 
 
-                    val dbReference = FirebaseDatabase.getInstance("https://videosappfirebase-default-rtdb.asia-southeast1.firebasedatabase.app/").getReference("Videos")
+                    val dbReference = FirebaseDatabase.getInstance().getReference("Videos")
                     dbReference.child(timestamp)
                         .setValue(hashMap)
                         .addOnSuccessListener {
